44e772e7fc2140735df0b7563227323d50619e50
Kimimaru <kimimaru@posteo.net>
2020-02-28 22:14:21 -0800

    Fixed a period interfering with the link to the code

--- TRBot/TRBotCore/Classes/Commands/OpenSourceCommand.cs
+++ TRBot/TRBotCore/Classes/Commands/OpenSourceCommand.cs
@@ -9,7 +9,7 @@ namespace TRBot
     {
         public override void ExecuteCommand(object sender, OnChatCommandReceivedArgs e)
         {
-            BotProgram.QueueMessage("This bot is libre software, licensed under AGPL v3.0. You can find the repository, which includes the source code and full license terms, here: https://github.com/teamradish/TRTwitchPlaysBot. Ask the streamer to learn more about their own modifications, if any, to the bot.");
+            BotProgram.QueueMessage("This bot is libre software, licensed under AGPL v3.0. You can find the repository, which includes the source code and full license terms, here: https://github.com/teamradish/TRTwitchPlaysBot Ask the streamer to learn more about their own modifications, if any, to the bot.");
         }
     }
 }
