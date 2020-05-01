e815af7eb300cb91ca5f9380e4b6c667ed7fc1c0
Kimimaru <kimimaru@posteo.net>
2020-05-01 04:09:44 -0700

    Fixed the working directory not always pointing to where the bot is launched from

--- TRBot/TRBotCore/Program.cs
+++ TRBot/TRBotCore/Program.cs
@@ -26,6 +26,12 @@ namespace TRBot
     {
         static void Main(string[] args)
         {
+            //Set current directory to where the program was run from
+            string assemblyLoc = typeof(Program).Assembly.Location;
+            Environment.CurrentDirectory = System.IO.Path.GetDirectoryName(assemblyLoc);
+            
+            Console.WriteLine($"Set current working directory to: {Environment.CurrentDirectory}");
+            
             using (BotProgram botProgram = new BotProgram())
             {
                 botProgram.Initialize();
