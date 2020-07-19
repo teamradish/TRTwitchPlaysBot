32d1f31a8c83681fecf4490383fcfb66d4dc1d5e
Kimimaru <kimimaru@posteo.net>
2020-07-19 03:52:12 -0700

    Wrote more tests

--- TRBot/TRBot.Tests/ParserTests.cs
+++ TRBot/TRBot.Tests/ParserTests.cs
@@ -11,7 +11,8 @@ namespace TRBot.Tests
         [TestCase("x", true, 60000)]
         [TestCase("jump", true, 60000)]
         [TestCase("y65s", true, 60000)] 
-        public void TestOneInput(string input, bool checkDur, int maxDur)
+        [TestCase("r5200ms", true, 5000)] 
+        public static void TestOneInput(string input, bool checkDur, int maxDur)
         {
             string inputRegex = Parser.BuildInputRegex(new string[] { input });
 
@@ -26,8 +27,12 @@ namespace TRBot.Tests
         [TestCase("a200ms #200ms b200ms", 600, 200, new string[] { "a", "#", "b" })]
         [TestCase("a1s", 1000, 200, new string[] { "a", "#", "b" })]
         [TestCase("a1000ms", 1000, 200, new string[] { "a", "#", "b" })]
+        [TestCase("_a2s", 2000, 200, new string[] { "a", "#", "b" })]
+        [TestCase("_a2000ms", 2000, 200, new string[] { "a", "#", "b" })]
         [TestCase("a1000ms+b1000ms", 1000, 200, new string[] { "a", "#", "b" })]
         [TestCase("a1000ms b1000ms", 2000, 200, new string[] { "a", "#", "b" })]
+        [TestCase("_a500ms b200ms #1s", 1700, 200, new string[] { "a", "#", "b" })]
+        [TestCase("_a500ms b200ms #1s -a1300ms", 3000, 200, new string[] { "a", "#", "b" })]
         public void TestDuration(string input, int expectedDur, int defaultDur, string[] validInputs)
         {
             string inputRegex = Parser.BuildInputRegex(validInputs);
@@ -35,6 +40,26 @@ namespace TRBot.Tests
             Parser.InputSequence inputSequence = Parser.ParseInputs(input, inputRegex, new Parser.ParserOptions(0, defaultDur, false));
 
             Assert.AreEqual(inputSequence.TotalDuration, expectedDur);
+        }
+
+        [TestCase("a", 1, new string[] { "a" })]
+        [TestCase("a+b", 2, new string[] { "a", "b" })]
+        [TestCase("a+b .", 3, new string[] { "a", "b", "." })]
+        [TestCase("a . b+a", 4, new string[] { "a", "b", "." })]
+        [TestCase("a+b start+a+b+x+y . . b . . a", 13, new string[] { "a", "b", "x", "y", "start", "." })]
+        public static void TestInputCount(string input, int expectedInputCount, string[] validInputs)
+        {
+            string inputRegex = Parser.BuildInputRegex(validInputs);
+
+            Parser.InputSequence inputSequence = Parser.ParseInputs(input, inputRegex, new Parser.ParserOptions(0, 200, false));
+
+            int inputCount = 0;
+            for (int i = 0; i < inputSequence.Inputs.Count; i++)
+            {
+                inputCount += inputSequence.Inputs[i].Count;
+            }
+            
+            Assert.AreEqual(inputCount, expectedInputCount);
         }
     }
 }
