7bb020ab5aeebdb8b804a6fade4d6dc306c7387c
Kimimaru <kimimaru@posteo.net>
2020-10-19 00:13:31 -0700

    Brought back and updated ParserPostProcess and ReverseParser

---
+++ TRBot/TRBot.Misc/ParserPostProcess.cs
@@ -0,0 +1,208 @@
+﻿/* This file is part of TRBot.
+ *
+ * TRBot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TRBot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with TRBot.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+using TRBot.ParserData;
+using TRBot.Consoles;
+using TRBot.VirtualControllers;
+
+namespace TRBot.Parsing
+{
+    /// <summary>
+    /// Handles post-processing inputs.
+    /// </summary>
+    public static class ParserPostProcess
+    {
+        /// <summary>
+        /// Tells if a <see cref="ParsedInputSequence"/> contains any restricted inputs.
+        /// </summary>
+        public static bool InputSequenceContainsRestrictedInputs(in ParsedInputSequence inputSequence, Dictionary<string, int> inputNames)
+        {
+            //Go through all inputs and find any restricted ones
+            List<List<ParsedInput>> allParsedInputs = inputSequence.Inputs;
+            for (int i = 0; i < allParsedInputs.Count; i++)
+            {
+                List<ParsedInput> inputs = allParsedInputs[i];
+
+                for (int j = 0; j < inputs.Count; j++)
+                {
+                    ParsedInput parsedInput = inputs[i];
+                    
+                    //Check if this input is restricted
+                    if (inputNames.ContainsKey(parsedInput.name) == true)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            //No restricted inputs
+            return false;
+        }
+
+        //Think of a faster way to do this; it's rather slow
+        //The idea is to look through and see the inputs that would be held at the same time and avoid the given combo
+        //One list is for held inputs with another for pressed inputs - the sum of their counts is compared with the invalid combo list's count
+        //Released inputs do not count
+        public static bool ValidateInputCombos(in ParsedInputSequence inputSequence, List<string> invalidCombo,
+            IVirtualControllerManager vControllerMngr, GameConsole gameConsole)
+        {
+            List<List<ParsedInput>> inputs = inputSequence.Inputs;
+
+            int controllerCount = vControllerMngr.ControllerCount;
+
+            //These dictionaries are for each controller port
+            Dictionary<int, List<string>> currentComboDict = new Dictionary<int, List<string>>(controllerCount);
+            Dictionary<int, List<string>> subComboDict = new Dictionary<int, List<string>>(controllerCount);
+            
+            for (int i = 0; i < controllerCount; i++)
+            {
+                IVirtualController controller = vControllerMngr.GetController(i);
+                if (controller.IsAcquired == false)
+                {
+                    continue;
+                }
+
+                //Add already pressed inputs from all controllers
+                for (int j = 0; j < invalidCombo.Count; j++)
+                {
+                    string inputName = invalidCombo[j];
+
+                    //Check if the button exists and is pressed
+                    if (gameConsole.GetButtonValue(inputName, out InputButton inputBtn) == false)
+                    {
+                        Console.WriteLine($"Warning: \"{inputName}\" is part of an invalid input combo but doesn't exist for {gameConsole.Name}.");
+                        continue;
+                    }
+                    else if (controller.GetButtonState(inputBtn.ButtonVal) == ButtonStates.Pressed)
+                    {
+                        if (currentComboDict.ContainsKey(i) == false)
+                        {
+                            currentComboDict[i] = new List<string>(invalidCombo.Count);
+                        }
+
+                        currentComboDict[i].Add(inputName);
+                        continue;
+                    }
+
+                    //Check if the axis exists and is pressed in any capacity
+                    if (gameConsole.GetAxisValue(inputName, out InputAxis inputAxis) == false)
+                    {
+                        Console.WriteLine($"Warning: \"{inputName}\" is part of an invalid input combo but doesn't exist for {gameConsole.Name}.");
+                        continue;
+                    }
+                    else if (controller.GetAxisState(inputAxis.AxisVal) != 0)
+                    {
+                        if (currentComboDict.ContainsKey(i) == false)
+                        {
+                            currentComboDict[i] = new List<string>(invalidCombo.Count);
+                        }
+                        
+                        currentComboDict[i].Add(inputName);
+                        continue;
+                    }
+                }
+            }
+
+            //If all these inputs are somehow pressed already, whatever we do now doesn't matter 
+            //However, returning false here would prevent any further inputs from working, so
+            //give a chance to check other inputs (such as releasing)
+            
+            for (int i = 0; i < inputs.Count; i++)
+            {
+                List<ParsedInput> inputList = inputs[i];
+
+                //Clear sublists
+                foreach (List<string> subList in subComboDict.Values)
+                {
+                    subList.Clear();
+                }
+                
+                for (int j = 0; j < inputList.Count; j++)
+                {
+                    ParsedInput input = inputList[j];
+
+                    //Get controller port and initialize
+                    int port = input.controllerPort;
+
+                    //Ensure a currentcombo entry is available for this port
+                    if (currentComboDict.ContainsKey(port) == false)
+                    {
+                        currentComboDict[port] = new List<string>(invalidCombo.Count);
+                    }
+
+                    //Ensure a subcombo entry is available for this port
+                    if (subComboDict.ContainsKey(port) == false)
+                    {
+                        subComboDict[port] = new List<string>(invalidCombo.Count);
+                    }
+
+                    //Current and sub combo lists
+                    List<string> currentCombo = currentComboDict[port];
+
+                    List<string> subCombo = subComboDict[port];
+
+                    //Check if this input is in the invalid combo
+                    if (invalidCombo.Contains(input.name) == true)
+                    {
+                        //If it's not a release input and isn't in the held or current inputs, add it
+                        if (input.release == false && subCombo.Contains(input.name) == false
+                            && currentCombo.Contains(input.name) == false)
+                        {
+                            subCombo.Add(input.name);
+                            
+                            //Check the count after adding
+                            if ((subCombo.Count + currentCombo.Count) == invalidCombo.Count)
+                            {
+                                return false;
+                            }
+                        }
+                        
+                        //For holds, use the held combo
+                        if (input.hold == true)
+                        {
+                            if (currentCombo.Contains(input.name) == false)
+                            {
+                                //Remove from the subcombo to avoid duplicates
+                                currentCombo.Add(input.name);
+                                subCombo.Remove(input.name);
+                                
+                                if ((currentCombo.Count + subCombo.Count) == invalidCombo.Count)
+                                {
+                                    return false;
+                                }
+                            }
+                        }
+                        //If released, remove from the current combo
+                        else if (input.release == true)
+                        {
+                            currentCombo.Remove(input.name);
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ TRBot/TRBot.Misc/ReverseParser.cs
@@ -0,0 +1,225 @@
+﻿/* This file is part of TRBot.
+ *
+ * TRBot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * TRBot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with TRBot.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+using TRBot.ParserData;
+using TRBot.Consoles;
+
+namespace TRBot.Parsing
+{
+    /// <summary>
+    /// A reverse parser that attempts to generate a string given a set of inputs.
+    /// </summary>
+    public static class ReverseParser
+    {
+        /// <summary>
+        /// Generates an input string given an <see cref="ParsedInputSequence"/>.
+        /// </summary>
+        /// <param name="inputSequence">The input sequence.</param>
+        /// <param name="gameConsole">The game console to reference.</param>
+        /// <returns>A string of the reverse parsed inputs.</returns>
+        public static string ReverseParse(in ParsedInputSequence inputSequence, GameConsole gameConsole)
+        {
+            List<List<ParsedInput>> inputsDList = inputSequence.Inputs;
+
+            //If the input isn't valid, then we can't reverse parse it because it couldn't be parsed!
+            //Likewise for a lack of inputs
+            if (inputSequence.InputValidationType != InputValidationTypes.Valid
+                || inputsDList == null || inputsDList.Count == 0)
+            {
+                return string.Empty;
+            }
+
+            //Initialize string builder for building the string
+            StringBuilder strBuilder = new StringBuilder(256);
+
+            for (int i = 0; i < inputsDList.Count; i++)
+            {
+                List<ParsedInput> inputList = inputsDList[i];
+
+                //Go through all inputs in the subsequence
+                for (int j = 0; j < inputList.Count; j++)
+                {
+                    ParsedInput input = inputList[j];
+
+                    //Add hold string
+                    if (input.hold == true)
+                    {
+                        strBuilder.Append(Parser.DEFAULT_PARSE_REGEX_HOLD_INPUT);
+                    }
+
+                    //Add release string
+                    if (input.release == true)
+                    {
+                        strBuilder.Append(Parser.DEFAULT_PARSE_REGEX_RELEASE_INPUT);
+                    }
+
+                    strBuilder.Append(input.name);
+
+                    //Add percent if it's an axis or the percent isn't the default
+                    if (input.percent != Parser.PARSER_DEFAULT_PERCENT
+                        || gameConsole.IsAxis(input) == true)
+                    {
+                        strBuilder.Append(input.percent).Append(Parser.DEFAULT_PARSE_REGEX_PERCENT_INPUT);
+                    }
+                    
+                    //Divide by 1000 to display seconds properly
+                    int duration = input.duration;
+                    if (input.duration_type == Parser.DEFAULT_PARSE_REGEX_SECONDS_INPUT)
+                    {
+                        duration /= 1000;
+                    }
+
+                    strBuilder.Append(duration);
+                    strBuilder.Append(input.duration_type);
+
+                    //Add plus string if there are more in the subsequence
+                    if (j < (inputList.Count - 1))
+                    {
+                        strBuilder.Append(Parser.DEFAULT_PARSE_REGEX_PLUS_INPUT);
+                    }
+                }
+
+                //Add a space for readability if there are more subsequences
+                if (i < (inputsDList.Count - 1))
+                {
+                    strBuilder.Append(' ');
+                }
+            }
+
+            string strBuilderVal = strBuilder.ToString();
+            string finalVal = Regex.Unescape(strBuilderVal);
+
+            return finalVal;
+        }
+
+        /// <summary>
+        /// Generates a natural sounding sentence given an <see cref="ParsedInputSequence"/>.
+        /// </summary>
+        /// <param name="inputSequence">The input sequence.</param>
+        /// <param name="gameConsole">The game console to reference.</param>
+        /// <returns>A string of the natural sentence.</returns>
+        /// <remarks>Wording is cut back to balance readability with the bot's character limit.</remarks>
+        public static string ReverseParseNatural(in ParsedInputSequence inputSequence, GameConsole gameConsole)
+        {
+            List<List<ParsedInput>> inputsDList = inputSequence.Inputs;
+
+            //If the input isn't valid, say so
+            if (inputSequence.InputValidationType != InputValidationTypes.Valid
+                || inputsDList == null || inputsDList.Count == 0)
+            {
+                return "Invalid input!";
+            }
+
+            //Initialize string builder for building the string
+            StringBuilder strBuilder = new StringBuilder(512);
+
+            for (int i = 0; i < inputsDList.Count; i++)
+            {
+                List<ParsedInput> inputList = inputsDList[i];
+
+                //Go through all inputs in the subsequence
+                for (int j = 0; j < inputList.Count; j++)
+                {
+                    ParsedInput input = inputList[j];
+
+                    //Handle hold
+                    if (input.hold == true)
+                    {
+                        if (i == 0 && j ==0) strBuilder.Append("Hold ");
+                        else strBuilder.Append("hold ");
+                    }
+                    //Handle release
+                    else if (input.release == true)
+                    {
+                        if (i == 0 && j ==0) strBuilder.Append("Release ");
+                        else strBuilder.Append("release ");
+                    }
+                    else if (gameConsole.IsBlankInput(input) == false)
+                    {
+                        if (i == 0 && j ==0) strBuilder.Append("Press ");
+                        else strBuilder.Append("press ");
+                    }
+
+                    //If waiting, say we should wait
+                    if (gameConsole.IsBlankInput(input) ==true)
+                    {
+                        if (i == 0 && j ==0) strBuilder.Append("Wait ");
+                        else strBuilder.Append("wait ");
+                    }
+                    else
+                    {
+                        //Add input name
+                        strBuilder.Append('\"').Append(input.name).Append('\"').Append(' ');
+
+                        //Add percent if it's an axis, the percent isn't the default, and not releasing
+                        if (input.release == false
+                            && (input.percent != Parser.PARSER_DEFAULT_PERCENT
+                            || gameConsole.IsAxis(input) == true))
+                        {
+                            strBuilder.Append(input.percent).Append("% ");
+                        }
+                    }
+                    
+                    //Divide by 1000 to display seconds properly
+                    int duration = input.duration;
+                    string durTypeStr = "msec";
+                    if (input.duration_type == Parser.DEFAULT_PARSE_REGEX_SECONDS_INPUT)
+                    {
+                        duration /= 1000;
+                        durTypeStr = "sec";
+                    }
+
+                    strBuilder.Append(duration).Append(' ').Append(durTypeStr);
+
+                    //Handle 1 (Ex. "second" instead of "seconds")
+                    if (duration != 1)
+                    {
+                        strBuilder.Append('s');
+                    }
+
+                    //Add plus string if there are more in the subsequence
+                    if (j < (inputList.Count - 1))
+                    {
+                        strBuilder.Append(" AND ");
+                    }
+                }
+
+                //Add a space for readability if there are more subsequences
+                if (i < (inputsDList.Count - 1))
+                {
+                    strBuilder.Append(", THEN ");
+                }
+                else
+                {
+                    strBuilder.Append('.');
+                }
+            }
+
+            string strBuilderVal = strBuilder.ToString();
+            string finalVal = Regex.Unescape(strBuilderVal);
+
+            return finalVal;
+        }
+    }
+}
